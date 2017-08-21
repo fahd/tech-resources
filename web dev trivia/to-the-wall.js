f96e77c73bb807f63df6bc7fe725c6165ce8609b
fahd <fahdnsheikh@gmail.com>
2017-08-21 12:37:14 -0700

    Update to-the-wall.js

--- web dev trivia/to-the-wall.js
+++ web dev trivia/to-the-wall.js
@@ -5,3 +5,6 @@ if ("a" in window) {
 console.log(a);
 
 // checks to see if the string is a property of the latter
+// this if clause shows up as true because line 2 creates a global, albeit undefined, value for a via hoisting,
+// since if blocks are not block scoped. if it it was a 'let', then we would throw an exception on line 5 
+// when we try to log a.
